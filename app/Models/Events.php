26adcb372579f0ac82a774c90f5813fcf1ccbfbb
Gasbiachraf <achrafgasbi2000@gmail.com>
2024-04-23 16:32:26 +0100

    create database

---
+++ app/Models/Events.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Events extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "organizer_id",
+        "description",
+        "date_start",
+        "time_start",
+        "date_end",
+        "time_end",
+        "location",
+        "price",
+        "image",
+        "ticket_price",
+    ];
+
+
+
+
+    public function users(){
+        return $this->belongsToMany(User::class , "events_users");
+    }
+    public function tickets(){
+        return $this->hasMany(Tickets::class);
+    }
+}
---
+++ database/migrations/2024_04_23_144747_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->foreignId("organizer_id");
+            $table->string("description");
+            $table->date("date_start");
+            $table->time("time_start");
+            $table->date("date_end");
+            $table->time("time_end");
+            $table->string("location");
+            $table->integer("price");
+            $table->string("image");
+            $table->string("ticket_price");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
